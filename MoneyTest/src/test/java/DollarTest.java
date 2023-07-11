8f58777688355689459bd3da40de28277202d5e0
WoeunJung <leena09123@naver.com>
2023-07-11 23:54:02 +0900

    feat: equals of Dollar obejct

--- MoneyTest/src/test/java/DollarTest.java
+++ MoneyTest/src/test/java/DollarTest.java
@@ -2,7 +2,7 @@ import org.example.Dollar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class DollarTest {
@@ -14,5 +14,12 @@ public class DollarTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+    @Test
+    @DisplayName("equals")
+    public void testEquality(){
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        // 해결되지 않은점: null이나 다른 객체들과 비교한다면?
     }
 }
