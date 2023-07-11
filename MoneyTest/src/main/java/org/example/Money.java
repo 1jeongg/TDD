8f773a13f5e58f876e8a5aa514458620766b37fe
WoeunJung <leena09123@naver.com>
2023-07-12 00:43:02 +0900

    feat: Dollar/Franc duplication

--- MoneyTest/src/main/java/org/example/Money.java
+++ MoneyTest/src/main/java/org/example/Money.java
@@ -1,7 +1,14 @@
 package org.example;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+    public static Dollar dollar(int amount){
+        return new Dollar(amount);
+    }
+    public static Franc franc(int amount){
+        return new Franc(amount);
+    }
+    public abstract Money times(int multiplier);
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount
